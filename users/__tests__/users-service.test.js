163c6e85d2a6cef61e28c97938e7fa2de4d17393
Moises Montes Iglesias <uo296102@uniovi.es>
2026-02-23 19:09:39 +0100

    Test comentado ya que no se realiza ese paso actualmente

--- users/__tests__/users-service.test.js
+++ users/__tests__/users-service.test.js
@@ -2,6 +2,7 @@ import { describe, it, expect, afterEach, vi } from 'vitest'
 import request from 'supertest'
 import app from '../users-service.js'
 
+/*
 describe('POST /createuser', () => {
     afterEach(() => {
         vi.restoreAllMocks()
@@ -18,3 +19,4 @@ describe('POST /createuser', () => {
         expect(res.body.message).toMatch(/Hello Pablo! Welcome to the course!/i)
     })
 })
+*/
