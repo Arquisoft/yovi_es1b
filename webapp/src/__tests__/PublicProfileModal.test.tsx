bc965a1b5bc8ada829e52593e503a58984012c4c
nahiaraSnchz <uo295645@uniovi.es>
2026-04-05 14:05:21 +0200

    Se elimina warning test

--- webapp/src/__tests__/PublicProfileModal.test.tsx
+++ webapp/src/__tests__/PublicProfileModal.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { PublicProfileModal } from '../components/modals/PublicProfileModal';
 import { gameService } from '../services/gameService';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
