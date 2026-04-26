85468789f0b4815d205eac0e5a2e732819079c89
diego <diegoabeijon4@gmail.com>
2026-04-26 23:01:16 +0200

    arreglo del safeToken

--- webapp/src/utils/sessionUtils.ts
+++ webapp/src/utils/sessionUtils.ts
@@ -95,10 +95,7 @@ const setOrClear = (key: string, value?: string | null, maxLength = 128) => {
   }
 };
 
-const isSafeToken = (value: string): boolean => {
-    // JWT format: three base64url segments separated by dots
-    return /^[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_]+\.[A-Za-z0-9\-_]+$/.test(value);
-};
+
 
 export const persistUserSession = (username: string, options: PersistUserSessionOptions) => {
   const name = normalizeStorageValue(username, 64);
